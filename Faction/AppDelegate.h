72498d0f5bfa36263c83c6127165d3abe98e7b7c
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-05-27 13:29:25 -0400

    Initial Commit

---
+++ Faction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Faction
+//
+//  Created by Sahil Gupta on 2015-05-27.
+//  Copyright (c) 2015 Sahil Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Faction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Faction
+//
+//  Created by Sahil Gupta on 2015-05-27.
+//  Copyright (c) 2015 Sahil Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
