79d8979ab51ea3eb88590c7be1b79175ae748159
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-05-27 15:55:00 -0400

    User can take a picture and see it at the bottom of the screen right away.

--- Faction/ViewController.h
+++ Faction/ViewController.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UIView *frameForCapture;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+- (IBAction)takePhoto:(id)sender;
 
 @end
 
