9eedd894ec17f5a92fe653a019ac49e8b89da3cc
Sahil Gupta <emailsahilgupta12@gmail.com>
2015-05-28 16:56:11 -0400

    Added input and session configs.

--- Faction/ViewController.m
+++ Faction/ViewController.m
@@ -17,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self startSession];
 
 }
 
@@ -27,6 +28,41 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
+}
+
+- (void) startSession {
+    
+    //Setting up the session for A/V IO.
+    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+    session.sessionPreset = AVCaptureSessionPresetPhoto;
+    
+    //Congifuring the device and device input
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo]; //Getting the default device which would be the back camera for an iPhone.
+    
+    NSError *error = nil;
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+    
+    if(!input) {
+        NSLog(@"An appropriate device cannot be located");
+    }
+    
+    //Adding the input device to the session.
+    if([session canAddInput:input]){
+        [session addInput:input];
+    }
+    
+    //May enable other features like auto-focusing, auto white balacing etc here.
+    if([device isFocusModeSupported:AVCaptureFocusModeContinuousAutoFocus]){
+        NSError *error = nil;
+        if([device lockForConfiguration:&error]){
+            device.focusMode = AVCaptureFocusModeContinuousAutoFocus;
+            [device unlockForConfiguration];
+        }
+    }
+    
+    
+    
+    
 }
 
 
